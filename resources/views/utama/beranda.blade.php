2bfb5b1c79fe1068171e379038566aa617a0cc9e
reynaldiarya <reynaldiarya.id@gmail.com>
2022-11-11 14:37:22 +0700

    add beranda

--- resources/views/utama/beranda.blade.php
+++ resources/views/utama/beranda.blade.php
@@ -8,45 +8,192 @@
     <title>Untitled</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="/assets/css/styles.min.css">
-    <link rel="stylesheet" href="/assets/css/custom.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/regular.min.css" integrity="sha512-aNH2ILn88yXgp/1dcFPt2/EkSNc03f9HBFX0rqX3Kw37+vjipi1pK3L9W08TZLhMg4Slk810sPLdJlNIjwygFw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/assets/css/Hero-Carousel-images.css">
+    <link rel="stylesheet" href="/assets/css/Navbar-Right-Links-icons.css">
 </head>
 
 <body>
     <header>
-    <nav class="navbar navbar-light navbar-expand-md py-3">
-        <div class="container"><a class="navbar-brand d-flex align-items-center" href="#"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-bezier">
-            <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
-            <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
-        </svg></span><span>Brand</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    <nav class="navbar navbar-light navbar-expand-md fixed-top py-3" style="background-color: rgba(255, 255, 255, 0.8);">
+        <div class="container"><a class="navbar-brand d-flex align-items-center" href="#"><img src="/assets/img/logo.png" width="161" height="29"></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-2"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-2">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item {{ request()->is('dashboard') ? 'active' : '' }}"><a class="nav-link" href="#">Toko</a></li>
-                    <li class="nav-item {{ request()->is('dashboard') ? 'active' : '' }}"><a class="nav-link" href="#">Tentang Kami</a></li>
-                    <li class="nav-item {{ request()->is('dashboard') ? 'active' : '' }}"><a class="nav-link" href="#">Hubungi Kami</a></li>
-                    <li class="nav-item {{ request()->is('dashboard') ? 'active' : '' }}"><a class="nav-link" href="#">Pelacakan</a></li>
-
-                </ul><a class="ms-md-4 nav-item {{ request()->is('dashboard') ? 'active' : '' }}" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+                    <li class="nav-item {{ request()->is('dashboard') ? 'active' : '' }}"><a class="nav-link text-black" href="#">TOKO</a></li>
+                    <li class="nav-item {{ request()->is('dashboard') ? 'active' : '' }}"><a class="nav-link text-black" href="#">TENTANG KAMI</a></li>
+                    <li class="nav-item {{ request()->is('dashboard') ? 'active' : '' }}"><a class="nav-link text-black" href="#">KONTAK KAMI</a></li>
+                    <li class="nav-item {{ request()->is('dashboard') ? 'active' : '' }}"><a class="nav-link text-black" href="#">PELACAKAN</a></li>
+                </ul><a class="text-black ms-md-5" role="button" href="#"><i class="fa-solid fa-cart-shopping"></i></a>
             </div>
         </div>
     </nav>
     </header>
     <main>
-        <div class="col-md-12">
-            <img style="width: 100%" src="/assets/img/beranda.png">
-        </div>
-    </main>
-    <footer class="text-center py-4">
-        <div class="container">
-            <div class="row row-cols-1 row-cols-lg-3">
-                <div class="col">
-                    <p class="text-muted my-2">Copyright&nbsp;© 2022 Brand</p>
+    <div class="carousel slide" data-bs-ride="carousel" id="carousel-1" style="height: 720px;">
+        <div class="carousel-inner h-100">
+            <div class="carousel-item active h-100"><img class="w-100 d-block position-absolute h-100 fit-cover" src="/assets/img/beranda.png" alt="Slide Image" style="z-index: -1;">
+                <div class="container d-flex flex-column justify-content-center h-100">
+                    <div class="row">
+                        <div class="col-md-6 col-xl-4">
+                            <h1 class="text-uppercase text-white fw-bold" style="font-size: 42px">Welcome To</h1>
+                            <h1 class="text-uppercase text-white fw-bold" style="font-size: 48px; font-family: Georgia, serif;">Grocetaria</h1>
+                        </div>
+                    </div>
                 </div>
+            </div>
+            <div class="carousel-item h-100"><img class="w-100 d-block position-absolute h-100 fit-cover" src="/assets/img/beranda-2.png" alt="Slide Image" style="z-index: -1;">
+                <div class="container d-flex flex-column justify-content-center h-100">
+                    <div class="row">
+                        <div class="col-md-6 col-xl-4">
+                            <h1 class="text-uppercase text-white fw-bold" style="font-size: 42px">Welcome To</h1>
+                            <h1 class="text-uppercase text-white fw-bold" style="font-size: 48px; font-family: Georgia, serif;">Grocetaria</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item h-100"><img class="w-100 d-block position-absolute h-100 fit-cover" src="/assets/img/beranda-3.png" alt="Slide Image" style="z-index: -1;">
+                <div class="container d-flex flex-column justify-content-center h-100">
+                    <div class="row">
+                        <div class="col-md-6 col-xl-4">
+                            <h1 class="text-uppercase text-white fw-bold" style="font-size: 42px">Welcome To</h1>
+                            <h1 class="text-uppercase text-white fw-bold" style="font-size: 48px; font-family: Georgia, serif;">Grocetaria</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev"><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next"><span class="visually-hidden">Next</span></a></div>
+        <ol class="carousel-indicators">
+            <li data-bs-target="#carousel-1" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#carousel-1" data-bs-slide-to="1"></li>
+            <li data-bs-target="#carousel-1" data-bs-slide-to="2"></li>
+        </ol>
+    </div>
+    </main>
+    <div class="container py-4 py-xl-5">
+        <div class="row mt-5 mb-3">
+            <div class="col">
+                <h2>Produk <span style="color: #60D05E">Terpopuler</span></h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 my-2">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card"><img class="card-img-top w-100 d-block">
+                            <div class="card-body">
+                                <h4 class="card-title">Sayur Kol 500g</h4>
+                                <p class="card-text">Rp 7.000,-</p>
+                                <p class="card-text">Terjual 250+</p><button class="btn" type="button"><i class="fa-solid fa-cart-shopping"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-2">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card"><img class="card-img-top w-100 d-block">
+                            <div class="card-body">
+                                <h4 class="card-title">Sayur Kol 500g</h4>
+                                <p class="card-text">Rp 7.000,-</p>
+                                <p class="card-text">Terjual 250+</p><button class="btn btn-primary" type="button">Button</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-2">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card"><img class="card-img-top w-100 d-block">
+                            <div class="card-body">
+                                <h4 class="card-title">Sayur Kol 500g</h4>
+                                <p class="card-text">Rp 7.000,-</p>
+                                <p class="card-text">Terjual 250+</p><button class="btn btn-primary" type="button">Button</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-2">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card"><img class="card-img-top w-100 d-block">
+                            <div class="card-body">
+                                <h4 class="card-title">Sayur Kol 500g</h4>
+                                <p class="card-text">Rp 7.000,-</p>
+                                <p class="card-text">Terjual 250+</p><button class="btn btn-primary" type="button">Button</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-2">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card"><img class="card-img-top w-100 d-block">
+                            <div class="card-body">
+                                <h4 class="card-title">Sayur Kol 500g</h4>
+                                <p class="card-text">Rp 7.000,-</p>
+                                <p class="card-text">Terjual 250+</p><button class="btn btn-primary" type="button">Button</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-2">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card"><img class="card-img-top w-100 d-block">
+                            <div class="card-body">
+                                <h4 class="card-title">Sayur Kol 500g</h4>
+                                <p class="card-text">Rp 7.000,-</p>
+                                <p class="card-text">Terjual 250+</p><button class="btn btn-primary" type="button">Button</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-2">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card"><img class="card-img-top w-100 d-block">
+                            <div class="card-body">
+                                <h4 class="card-title">Sayur Kol 500g</h4>
+                                <p class="card-text">Rp 7.000,-</p>
+                                <p class="card-text">Terjual 250+</p><button class="btn btn-primary" type="button">Button</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-2">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card"><img class="card-img-top w-100 d-block">
+                            <div class="card-body">
+                                <h4 class="card-title">Sayur Kol 500g</h4>
+                                <p class="card-text">Rp 7.000,-</p>
+                                <p class="card-text">Terjual 250+</p><button class="btn btn-primary" type="button">Button</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="text-center py-4" style="padding: 50px 50px; background-color: rgba(65, 65, 65, 0.13)">
+        <div class="container-fluid">
+            <div class="row row-cols-1 row-cols-lg-3 my-auto">
+                <div class="col my-5"><img src="/assets/img/logo.png" style="width: 80%"></div>
                 <div class="col"></div>
-                <div class="col">
-                    <ul class="list-inline my-2">
-                        <li class="list-inline-item"><a class="link-secondary" href="#">Privacy Policy</a></li>
-                        <li class="list-inline-item"><a class="link-secondary" href="#">Terms of Use</a></li>
+                <div class="col my-5">
+                    <ul class="navbar-nav ms-auto" style="text-align: left;">
+                        <li class="nav-item"><i class="fa-solid fa-map-location-dot"></i>&nbsp;Jalan Kejawan Putih 4, Surabaya</li>
+                        <li class="nav-item"><i class="fa-regular fa-envelope"></i>&nbsp;grocetaria98@gmail.com</li>
+                        <li class="nav-item"><i class="fa-solid fa-phone"></i>&nbsp;082 243 227 694</li>
+                        <li class="nav-item"><i class="fa-brands fa-instagram"></i>&nbsp;@grocetariashop</li>
                     </ul>
                 </div>
             </div>
